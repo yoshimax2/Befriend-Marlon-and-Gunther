e5600e627004ec5245de2d8763cc8541b4f650fc
Jesse Plamondon-Willard <Pathoschild@users.noreply.github.com>
2019-02-25 20:46:55 -0500

    update manifest & version

--- BefriendMarlonAndGunther/Properties/AssemblyInfo.cs
+++ BefriendMarlonAndGunther/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System.Reflection;
 
 [assembly: AssemblyTitle("BefriendMarlonAndGunther")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.5")]
+[assembly: AssemblyFileVersion("1.0.5")]
